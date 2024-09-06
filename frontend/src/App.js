b289e778934a98d52b06ac975597e509e08dbb1b
Imran Ahamed <inahamed1995@gmail.com>
2024-09-06 00:11:33 -0700

    separate lists onto different pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,58 @@
 
+// import React from 'react';
+// import VolunteersList from './components/VolunteersList';
+// import DonationsList from './components/DonationsList';
+// import ProjectsList from './components/ProjectsList';
+// import EventsList from './components/EventsList';
+
+
+// function App() {
+  
+//   return (
+//     <div>
+//       <h1>Welcome to Non-Profit Management System</h1>
+//       <VolunteersList />
+//       <DonationsList />
+//       <ProjectsList />
+//       <EventsList />
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import VolunteersList from './components/VolunteersList';
 import DonationsList from './components/DonationsList';
 import ProjectsList from './components/ProjectsList';
 import EventsList from './components/EventsList';
-
+// import AddEvent from './components/AddEvent';
 
 function App() {
-  
   return (
-    <div>
-      <h1>Welcome to Non-Profit Management System</h1>
-      <VolunteersList />
-      <DonationsList />
-      <ProjectsList />
-      <EventsList />
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li><Link to="/volunteers">Volunteers</Link></li>
+            <li><Link to="/donations">Donations</Link></li>
+            <li><Link to="/projects">Projects</Link></li>
+            <li><Link to="/events">Events</Link></li>
+            {/* <li><Link to="/add-event">Add New Event</Link></li> */}
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/volunteers" element={<VolunteersList />} />
+          <Route path="/donations" element={<DonationsList />} />
+          <Route path="/projects" element={<ProjectsList />} />
+          <Route path="/events" element={<EventsList />} />
+          {/* <Route path="/add-event" element={<AddEvent />} /> */}
+          <Route path="/" element={<h1>Welcome to the Volunteer Management Application</h1>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
